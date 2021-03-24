4d95e9f95156b6b3e144c11d935210303cabf474
Prabagaran <nikprabha90@gmail.com>
2021-03-24 16:21:27 +0530

    fea: use cases added in tests

---
+++ CodeKataTests/TennisGameTests.swift
@@ -0,0 +1,41 @@
+//
+//  TennisGameTests.swift
+//  CodeKataTests
+//
+//  Created by Prabhakar G on 24/03/21.
+//  Copyright © 2021 Prabhakar. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class TennisGameTests: XCTest {
+    
+    func testPointsShouldBeLoveLoveWhenStartOfTheGame() {
+        
+    }
+    
+    func testPointShouldBeFifteenLoveWhenPlayer1ScoredFirst() {
+        
+    }
+    
+    func testPointShouldBeThirtyLoveWhenPlayer1ScoredSecondTime() {
+        
+    }
+    
+    func testPointShouldBeFortyLoveWhenPlayer1ScoredThirdTimeContinuosly() {
+        
+    }
+    
+    func testPointShoudBeAdvantageWhenBothPlayersScoreLeastThreeTimesAndPlayers1HasPointMoreThanPlayer2() {
+        
+    }
+    
+    func testPointsShouldBeDeuceWhenBothPlayersScoresLeastThreeTimesAndPlayersPointsAreEqual() {
+        
+    }
+    
+    func testGameShouldBeWonByPlayer1WhenPlayer1ScoredFourTotalWithTwoPointsMoreThanPlayer2() {
+        
+    }
+}
