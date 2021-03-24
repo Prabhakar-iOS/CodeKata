066722f18a4436b16d9cee1943a540d00896393e
Prabagaran <nikprabha90@gmail.com>
2021-03-24 16:07:22 +0530

    chore: base setup

---
+++ CodeKata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeKata
+//
+//  Created by Prabhakar G on 24/03/21.
+//  Copyright © 2021 Prabhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
